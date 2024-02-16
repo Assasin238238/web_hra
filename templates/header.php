348ab7dc4be69daee3c0c5c52ea118c2c03291af
Assasin238238 <harnach2006@gmail.com>
2024-02-16 08:24:20 +0100

    new
    new

---
+++ templates/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+	<meta charset="UTF-8">
+	<title>Mùj web</title>
+	<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<header>
+			<h1>Mùj web</h1>
